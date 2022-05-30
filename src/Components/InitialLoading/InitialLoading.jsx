c0c9743051f21d7a3dfbf7d43b52de34e252f972
Milan <raymilan49@gmail.com>
2022-05-30 16:02:30 +0530

    ht adjust 17

--- src/Components/InitialLoading/InitialLoading.jsx
+++ src/Components/InitialLoading/InitialLoading.jsx
@@ -4,22 +4,21 @@ import loadingScreen from "../../Extra/grayLogo.png"
 
 function InitialLoading() {
     return (
-        <></>
-        // <div className="PostContainer" style={{
-        //     width: "600px",
-        //     height: "calc(100vh - 150px )",
-        //     display: "flex",
-        //     justifyContent: "center",
-        //     alignItems: "center",
-        //     border: "none",
-        //     background: "transparent",
-        //     // background: "black"
-        // }}>
-        //     <img style={{
-        //         objectFit: "cover",
-        //         width: "8.5%",
-        //     }} src={loadingScreen} alt="loadingScreen" />
-        // </div>
+        <div className="PostContainer" style={{
+            width: "600px",
+            height: "calc(100vh - 150px )",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "none",
+            background: "transparent",
+            // background: "black"
+        }}>
+            <img style={{
+                objectFit: "cover",
+                width: "8.5%",
+            }} src={loadingScreen} alt="loadingScreen" />
+        </div>
     )
 }
 
