656262ab791395aef194417485bb617791b07fc0
Milan <raymilan49@gmail.com>
2022-05-30 16:25:59 +0530

    ht adjust 24

--- src/Components/InitialLoading/InitialLoading.jsx
+++ src/Components/InitialLoading/InitialLoading.jsx
@@ -12,7 +12,6 @@ function InitialLoading() {
             alignItems: "center",
             border: "none",
             background: "transparent",
-            background: "black"
         }}>
             <img style={{
                 objectFit: "cover",
