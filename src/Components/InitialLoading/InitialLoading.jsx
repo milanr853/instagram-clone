1ac42aca3e2fdbfa14fed5d20d46350313778aaf
Milan <raymilan49@gmail.com>
2022-05-30 16:13:23 +0530

    ht adjust 20

--- src/Components/InitialLoading/InitialLoading.jsx
+++ src/Components/InitialLoading/InitialLoading.jsx
@@ -6,7 +6,7 @@ function InitialLoading() {
     return (
         <div className="PostContainer" style={{
             width: "600px",
-            height: "calc(100vh - 260px )",
+            height: "calc(100vh - 300px )",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
